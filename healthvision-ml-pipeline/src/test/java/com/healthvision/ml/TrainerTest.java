1d4a7b9c9e6c86c3dbf9c79fdb14e13c4e54b46e
Prakash Arjunan <prakashguy50@gmail.com>
2025-03-28 13:57:25 +0530

    Mvn setup

--- healthvision-ml-pipeline/src/test/java/com/healthvision/ml/TrainerTest.java
+++ healthvision-ml-pipeline/src/test/java/com/healthvision/ml/TrainerTest.java
@@ -1,16 +1,16 @@
 package com.healthvision.ml;
 
 import org.junit.jupiter.api.Test;
-import java.io.File;
 import static org.junit.jupiter.api.Assertions.*;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
+import java.io.File;
 
 public class TrainerTest {
     @Test
     public void testDataLoading() throws Exception {
         CSVLoader loader = new CSVLoader();
-        loader.setSource(new File("datasets/iris.csv"));
+        loader.setSource(new File("datasets/iris.csv"));  // Relative path from project root
         Instances data = loader.getDataSet();
         assertEquals(150, data.numInstances());
         assertEquals(5, data.numAttributes());
