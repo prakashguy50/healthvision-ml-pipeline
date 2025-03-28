1bab1f90f8df3b704d665422e480f0855064dd98
Prakash Arjunan <prakashguy50@gmail.com>
2025-03-28 13:50:05 +0530

    Added missing headers

--- healthvision-ml-pipeline/src/main/java/com/healthvision/ml/GCSClient.java
+++ healthvision-ml-pipeline/src/main/java/com/healthvision/ml/GCSClient.java
@@ -1,6 +1,8 @@
 package com.healthvision.ml;
 
 import com.google.cloud.storage.*;
+import java.io.FileInputStream; // Import for FileInputStream
+import java.io.IOException; // Import for IOException
 
 public class GCSClient {
     private final Storage storage;
@@ -11,13 +13,17 @@ public class GCSClient {
         this.bucketName = System.getProperty("gcs.bucket");
     }
     
-    public void uploadModel(String modelPath) throws Exception {
+    public void uploadModel(String modelPath) throws IOException {
         BlobId blobId = BlobId.of(bucketName, "models/" + modelPath);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-        storage.create(blobInfo, new FileInputStream(modelPath).readAllBytes());
+        
+        // Ensure FileInputStream is properly closed after use
+        try (FileInputStream fileInputStream = new FileInputStream(modelPath)) {
+            storage.create(blobInfo, fileInputStream.readAllBytes());
+        }
     }
     
-    public void downloadModel(String modelName, String localPath) throws Exception {
+    public void downloadModel(String modelName, String localPath) throws IOException {
         Blob blob = storage.get(bucketName, "models/" + modelName);
         blob.downloadTo(java.nio.file.Paths.get(localPath));
     }
